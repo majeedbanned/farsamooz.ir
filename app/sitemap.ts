d413f4b125c667f68ae3fff870a1c10886dc4091
majid <majeed.ghasemi@gmail.com>
2023-09-14 22:26:20 +0330

    image correction

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -20,14 +20,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.5,
     },
-    ,
+    
     {
         url: 'https://farsamooz.ir/agents',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
     },
-    ,
+    
     {
         url: 'https://farsamooz.ir/contact',
       lastModified: new Date(),
